fce72ae2f9e4d4652da4773b3ce4845c8ed6e6fa
lakshu <ced15i004@iiitdm.ac.in>
2022-02-21 17:55:39 +0530

    Resolved the issue for Image Picking

--- app/src/main/java/com/example/permission/imagepick.kt
+++ app/src/main/java/com/example/permission/imagepick.kt
@@ -1,21 +1,25 @@
 package com.example.permission
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
-import android.os.Build
-import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -26,11 +30,29 @@ import androidx.compose.ui.unit.sp
 fun imagepick() {
     var imageuri by remember { mutableStateOf<Uri?>(null) }
     val context = LocalContext.current
+    var myimagelist: List<Uri> = listOf()
+    var newlist = remember {
+        mutableStateOf(emptyList<Bitmap>())
+    }
     val bitmap = remember { mutableStateOf<Bitmap?>(null) }
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent(),
         onResult = {
             imageuri = it
+        })
+    val getmultiple = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetMultipleContents(),
+        onResult = {
+            myimagelist = it
+            it.forEach {
+                Log.i("MainActivity", "Value = ${it}")
+            }
+            newlist.value = myimagelist.mapIndexed { _, uri ->
+                        calculatebitmap(context,uri)}
+            newlist.value.forEach {
+                Log.i("mainActivity","Values as BItmao =${it}")
+            }
+
         })
     Column(
         modifier = Modifier
@@ -41,26 +63,104 @@ fun imagepick() {
         Text(text = "Pick Your Images ", fontSize = 24.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.padding(15.dp))
         imageuri?.let {
-            if(Build.VERSION.SDK_INT < 28){
-                bitmap.value = MediaStore.Images.Media.getBitmap(context.contentResolver,it)
-            }else {
-                val source = ImageDecoder.createSource(context.contentResolver,it)
-                bitmap.value = ImageDecoder.decodeBitmap(source)
-            }
+            val source = ImageDecoder.createSource(context.contentResolver, it)
+            bitmap.value = ImageDecoder.decodeBitmap(source)
             bitmap.value?.let {
-                Image(bitmap = it.asImageBitmap(), contentDescription = null,
+                Image(
+                    bitmap = it.asImageBitmap(), contentDescription = null,
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(15.dp)
-                        .height(500.dp)
-                    )
+                        .height(450.dp)
+                )
             }
         }
-        Spacer(modifier = Modifier.padding(15.dp))
-        Button(onClick = {
-            launcher.launch("image/")
-        }) {
-            Text(text = "Pick Image", fontSize = 25.sp)
+        Spacer(modifier = Modifier.padding(5.dp))
+        Text(text = "Pick Your Images ", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.padding(5.dp))
+//
+        Button(
+            onClick = {
+                launcher.launch("image/")
+            },
+            modifier = Modifier
+                .padding(5.dp)
+        ) {
+            Text(text = "Pick Image", fontSize = 22.sp)
         }
+        Spacer(modifier = Modifier.padding(5.dp))
+        LazyRow(
+        modifier = Modifier
+                .padding(10.dp).fillMaxWidth()){
+            itemsIndexed(newlist.value){ _,image ->
+                Image(
+                    bitmap = image.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(120.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                )
+            }
+        }
+        Spacer(modifier = Modifier.padding(5.dp))
+        Button(onClick = {
+            getmultiple.launch("image/")
+
+        }) {
+            Text(text = "Pick Multiple Image", fontSize = 22.sp)
+        }
+  
+    }
+}
+fun calculatebitmap(ctx:Context,it:Uri): Bitmap {
+    val soutc = ImageDecoder.createSource(ctx.contentResolver, it)
+    return ImageDecoder.decodeBitmap(soutc)
+}
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun getbitmaps(context: Context, list: List<Uri>) {
+    val mybitmap = remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+
+    val newlist = list.map {
+        ImageDecoder.decodeBitmap(
+            ImageDecoder.createSource(context.contentResolver, it)
+        )
+    }
+    Column(
+        modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+                .padding(10.dp)
+        ) {
+            items(newlist) { image ->
+                mybitmap.value = image
+                mybitmap.value?.let {
+                    Image(
+                        bitmap = it.asImageBitmap(),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .size(120.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                    )
+                }
+//                    mybitmap.value = ImageDecoder.decodeBitmap(
+//                        ImageDecoder.createSource(
+//                            context.contentResolver,
+//                            it
+//                        )
+//
+
+            }
+        }
+
     }
 }
